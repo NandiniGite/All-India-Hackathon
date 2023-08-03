6089ad5eab5629b0f69d0b469e8ae8beedc3576c
NandiniGite <98202333+NandiniGite@users.noreply.github.com>
2023-08-03 11:22:13 +0530

    Add files via upload

---
+++ Display.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Display.css'
+const Display = () => {
+  return (
+    <>
+    <div className='main'>
+      <h2 className='header'>Yogasanas</h2>
+    </div>
+    <div className='main'>
+      <h3 className='quote'>"Yoga means addition – addition of energy, strength and beauty to body, mind and soul.”</h3>
+    </div>
+    </>
+  )
+}
+
+export default Display
---
+++ Sliderdata.js
@@ -0,0 +1,19 @@
+import image1 from '../../assets/images/girl doing sury 0.png'
+import image2 from '../../assets/images/girl doing tree 1.png'
+import image3 from '../../assets/images/girl doing warr 0.png'
+
+export const SliderData = [
+    {
+      image:image1,
+      name:"Suryanamaskar"
+    },
+    {
+      image:image2,
+      name:"Vrksasana"
+    },
+    {
+      image:image3,
+      name:"Virabhadrasana"
+    },
+    
+  ];
